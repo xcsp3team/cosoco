82d1669f8f10f9eeabbcd271ba6a8218805c5577
Gilles Audemard <audemard@cril.fr>
2022-11-17 10:21:56 +0100

    update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,3 @@
-rm -rf cmake-build-debug
 rm -rf main/cosoco
 cd ../XCSP3-CPP-Parser
 cmake --build . --target clean -- -j 8
@@ -6,3 +5,5 @@ rm -rf lib/libxcsp3parser.a
 cd ../cosoco/
 cmake --build . --target clean -- -j 8
 rm -rf CMakeCache.txt  CMakeFiles cmake_install.cmake Makefile
+rm -rf cmake-build-debug
+
