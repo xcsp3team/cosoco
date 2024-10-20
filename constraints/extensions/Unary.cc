47a44b96a4cfb43af1b170e331908873b6496662
Gilles Audemard <gilles.audemard@univ-artois.fr>
2024-10-20 09:40:27 +0200

    reduce calls to Unary

--- constraints/extensions/Unary.cc
+++ constraints/extensions/Unary.cc
@@ -22,22 +22,29 @@ Unary::Unary(Problem &p, std::string n, Variable *xx, const vec<int> &vals, bool
 }
 
 
-Cosoco::State Cosoco::Unary::status() { return done ? CONSISTENT : UNDEF; }
+Cosoco::State Cosoco::Unary::status() { return UNDEF; }
 
 
-void Cosoco::Unary::reinitialize() { done = false; }
+void Cosoco::Unary::reinitialize() { }
 
 //----------------------------------------------------------
 // Filtering
 //----------------------------------------------------------
 
 bool Cosoco::Unary::filter(Cosoco::Variable *dummy) {
+    if(done) {
+        // If new call => because restarts and entail is deleted...
+        solver->entail(this);
+        return true;
+    }
     done = true;
 
     if(areSupports) {
-        if(values.size() == 1)
-            return solver->assignToVal(x, values[0]);
-
+        if(values.size() == 1) {
+            bool ret = solver->assignToVal(x, values[0]);
+            solver->entail(this);
+            return ret;
+        }
 
         for(int idv : reverse(x->domain)) {
             int v = x->domain.toVal(idv);
