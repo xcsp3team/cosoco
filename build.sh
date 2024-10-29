0d043a5876b9adda72ab7f6728dd2865db938712
Tomo Sona Lawa <tomosonalawa@gmail.com>
2024-10-29 18:19:16 +0100

    added framework for testing using gtest (if gtest is not present, the tests will not be compiled)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd ../XCSP3-CPP-Parser
 ./build.sh
 cd ../cosoco/
-cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" .
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_RUNTIME_OUTPUT_DIRECTORY="`pwd`/main" -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="`pwd`/lib" -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY="`pwd`/lib" -G "Unix Makefiles" .
 #cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" .
 cmake --build . --target cosoco -- -j 8
---
+++ test/Integration.cc
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include "Solver.h"
+#include "core/Problem.h"
+#include "core/Variable.h"
+#include "core/domain/DomainRange.h"
+#include "solver/utils/Options.h"
+
+TEST(IntegrationTest, oneVarNoConstraint) {
+    Cosoco::options::createOptions();
+
+    Cosoco::Problem     p("oneVarNoConstraint");
+    Cosoco::DomainRange domain(0, 4);
+    p.createVariable(std::string("MyOnlyVariable"), domain);
+    Cosoco::Solver                       solver(p);
+    Cosoco::vec<Cosoco::RootPropagation> assumps;
+    p.delayedConstruction();
+    try {
+        auto result = solver.solve(assumps);
+        EXPECT_EQ(result, R_SAT);
+    } catch(std::exception &e) {
+        printf("%s", e.what());
+        FAIL();
+    }
+}
