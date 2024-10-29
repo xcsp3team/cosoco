7bfae812f403803084a8ca8b9ee4c506311ab5c1
Tomo Sona Lawa <tomosonalawa@gmail.com>
2024-10-29 23:15:18 +0100

    Added new test with a single constraint

--- test/Integration.cc
+++ test/Integration.cc
@@ -1,18 +1,38 @@
 #include <gtest/gtest.h>
 
+#include "constraints/primitives/GEUnary.h"
 #include "core/Problem.h"
 #include "core/Variable.h"
 #include "core/domain/DomainRange.h"
 #include "solver/Solver.h"
 #include "solver/restarts/Restart.h"
-#include "solver/utils/Options.h"
+#include "test/Utils.h"
 
 TEST(IntegrationTest, oneVarNoConstraint) {
-    Cosoco::options::createOptions();
+    Cosoco::OptionDefaultAndCleaner _defaultOptions;
 
     Cosoco::Problem     p("oneVarNoConstraint");
     Cosoco::DomainRange domain(0, 4);
     p.createVariable(std::string("MyOnlyVariable"), domain);
+    Cosoco::Solver                       solver(p);
+    Cosoco::vec<Cosoco::RootPropagation> assumps;
+    p.delayedConstruction();
+    try {
+        auto result = solver.solve(assumps);
+        EXPECT_EQ(result, R_SAT);
+    } catch(std::exception &e) {
+        printf("%s", e.what());
+        FAIL();
+    }
+}
+
+TEST(IntegrationTest, oneVarOneConstraint) {
+    Cosoco::OptionDefaultAndCleaner _defaultOptions;
+
+    Cosoco::Problem     p("oneVarNoConstraint");
+    Cosoco::DomainRange domain(0, 4);
+    auto               *var = p.createVariable(std::string("MyOnlyVariable"), domain);
+    p.addConstraint(std::make_unique<Cosoco::GEUnary>(p, "MyOnlyConstraint", var, 3));
     Cosoco::Solver                       solver(p);
     Cosoco::vec<Cosoco::RootPropagation> assumps;
     p.delayedConstruction();
