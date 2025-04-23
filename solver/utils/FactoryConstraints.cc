73159060c9e8f386efe122c1aad2dc521952d4e6
Gilles Audemard <gilles.audemard@univ-artois.fr>
2025-04-23 17:50:36 +0200

    Ordered with length var ok

--- solver/utils/FactoryConstraints.cc
+++ solver/utils/FactoryConstraints.cc
@@ -514,6 +514,21 @@ void FactoryConstraints::createConstraintSum(Problem *p, std::string name, vec<V
     coefs2.copyTo(coeffs);
     assert(coeffs.size() == vars.size());
 
+    // Need order on coefficients
+    for(int i = 0; i < vars.size(); i++) {
+        int pos = i;
+        for(int j = i + 1; j < vars.size(); j++) {
+            if(coeffs[j] < coeffs[pos])
+                pos = j;
+        }
+        int tmp     = coeffs[i];
+        coeffs[i]   = coeffs[pos];
+        coeffs[pos] = tmp;
+        Variable *x = vars[i];
+        vars[i]     = vars[pos];
+        vars[pos]   = x;
+    }
+
 
     // remove coef 0
     int i = 0, j = 0;
@@ -653,20 +668,19 @@ void FactoryConstraints::createConstraintOrdered(Problem *p, std::string name, v
 
 void FactoryConstraints::createConstraintOrdered(Problem *p, std::string name, vec<Variable *> &vars, vec<Variable *> &lengths,
                                                  OrderType op) {
-    vec<Variable *> tmp;
-    tmp.growTo(3);
-    vec<int> coeffs;
-    coeffs.push(1);
-    coeffs.push(1);
-    coeffs.push(-1);
-
     for(int i = 0; i < vars.size() - 1; i++) {
-        std::cout << vars[i]->_name << " " << lengths[i]->_name << " -" << vars[i + 1]->_name << std::endl;
+        vec<int> coeffs;
+        coeffs.push(1);
+        coeffs.push(1);
+        coeffs.push(-1);
+        vec<Variable *> tmp;
+        tmp.push(vars[i]);
+        tmp.push(lengths[i]);
+        tmp.push(vars[i + 1]);
         tmp[0] = vars[i];
         tmp[1] = lengths[i];
         tmp[2] = vars[i + 1];
         createConstraintSum(p, name, tmp, coeffs, 0, op);
-        p->constraints.last()->display(true);
     }
 }
 
