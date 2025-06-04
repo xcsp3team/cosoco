d3999c70c24b7eee670759667b794025b5d84053
Gilles <gilles.audemard@gmail.com>
2025-06-04 11:57:41 +0200

    update readme, build and clean.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 cd ../XCSP3-CPP-Parser
 ./build.sh
+cd ../pfactory
+./bootstrap
+make
 cd ../cosoco/
 cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" .
 #cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" .
--- clean.sh
+++ clean.sh
@@ -2,6 +2,8 @@ rm -rf main/cosoco
 cd ../XCSP3-CPP-Parser
 cmake --build . --target clean -- -j 8
 rm -rf lib/libxcsp3parser.a
+cd ../pfactory
+make clean
 cd ../cosoco/
 cmake --build . --target clean -- -j 8
 rm -rf CMakeCache.txt  CMakeFiles cmake_install.cmake Makefile
