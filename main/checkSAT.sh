2a02eede2fa230e38734b6db2cb0336628f57b43
Gilles Audemard <gilles.audemard@univ-artois.fr>
2025-09-17 16:30:03 +0200

    change checker of solution

--- main/checkSAT.sh
+++ main/checkSAT.sh
@@ -1,3 +1,3 @@
 #!/bin/csh
 
-./cosoco $argv -model=1 |grep -e "^v"| sed -e 's/^v\(.*\)/\1/'| java -cp ~/csp/XCSP3-Java-Tools/target/xcsp3-solutionChecker-2.3.jar org.xcsp.parser.callbacks.SolutionChecker $argv[1]
+./cosoco $argv -model=2 |grep -e "^v"| sed -e 's/^v\(.*\)/\1/'| java -cp ~/csp/XCSP3-Java-Tools/target/xcsp3-solutionChecker-2.5.1.jar org.xcsp.parser.callbacks.SolutionChecker $argv[1]
