57d4514ffad0675be58f2c7a43f6eef331b0e911
Gilles Audemard <gilles.audemard@gmail.com>
2026-03-27 15:54:55 +0100

    start knapsack

---
+++ constraints/globals/packing/Knapsack.cc
@@ -0,0 +1,187 @@
+//
+// Created by audemard on 27/03/2026.
+//
+
+#include "Knapsack.h"
+
+#include "solver/Solver.h"
+
+using namespace Cosoco;
+
+//----------------------------------------------
+// Check validity and correct definition
+//----------------------------------------------
+
+int sum(vec<int> &tuple, vec<int> &coefs) {
+    int sum = 0;
+    for(int posx = 0; posx < coefs.size(); posx++) sum += tuple[posx];
+    return sum;
+}
+
+bool Knapsack::isSatisfiedBy(vec<int> &tuple) { return sum(tuple, weights) <= wlimit && sum(tuple, profits) >= plimit; }
+
+bool KnapsackVARW::isSatisfiedBy(vec<int> &tuple) { return sum(tuple, weights) <= tuple.last() && sum(tuple, profits) >= plimit; }
+
+bool KnapsackVARP::isSatisfiedBy(vec<int> &tuple) { return sum(tuple, weights) <= wlimit && sum(tuple, profits) >= tuple.last(); }
+bool KnapsackVARWP::isSatisfiedBy(vec<int> &tuple) {
+    return sum(tuple, weights) <= tuple[tuple.size() - 2] && sum(tuple, profits) >= tuple.last();
+}
+
+bool Knapsack::isCorrectlyDefined() {
+    return weights.size() == vars.size() && profits.size() == vars.size() && weights.min() >= 0 && profits.min() >= 0;
+}
+
+//----------------------------------------------
+// Filrtering
+//----------------------------------------------
+void Knapsack::recomputeBounds() {
+    wmin = wmax = pmin = pmax = 0;
+    for(int posx = 0; posx < scope.size(); posx++) {
+        wmin += weights[posx] * scope[posx]->minimum();
+        wmax += weights[posx] * scope[posx]->maximum();
+        pmin += profits[posx] * scope[posx]->minimum();
+        pmax += profits[posx] * scope[posx]->maximum();
+    }
+}
+
+bool Knapsack::filter(Variable *dummy) {
+    if(basic)
+        recomputeBounds();
+    if(wmax <= wlimit && pmin >= plimit)
+        return basic ? solver->entail(this) : true;
+    if(wmin > wlimit || pmax < plimit)
+        return false;
+    if(wmax > wlimit) {   // otherwise nothing to do
+        for(int posx = 0; posx < vars.size(); posx++) {
+            Variable *x = vars[posx];
+            if(x->size() == 1 || weights[posx] == 0)
+                continue;
+            int wcoeff = weights[posx];
+            int pcoeff = profits[posx];
+            wmax -= wcoeff * x->maximum();
+            pmax -= pcoeff * x->maximum();
+            long wmini = wmin - wcoeff * x->minimum();   // we remove the contribution of the variable we consider
+            long pmini = pmin - pcoeff * x->minimum();
+
+            dom.removeValues(GT, wlimit - wmini, wcoeff);
+            if(profits[posx] == 0)
+                continue;
+
+            while(true) {
+                int v              = x->maximum();
+                int nPossibleMoves = (int)std::floor((wlimit - wmini - wcoeff * v) / minWeight);   // 1 if 0 present ? why ?
+                if((pmini + nPossibleMoves * maxProfit) + pcoeff * v < plimit) {                   //
+                    if(solver->delVal(x, v) == false)
+                        return false;
+                } else
+                    break;
+            }
+            wmax += wcoeff * x->maximum();
+            pmax += pcoeff * x->maximum();
+            if(wmax <= wlimit)
+                break;
+        }
+    }
+    if(pmin < plimit) {   // otherwise nothing to do
+        for(int posx = 0; posx < vars.size(); posx++) {
+            Variable *x = vars[posx];
+            if(x->size() == 1 || profits[posx] == 0)
+                continue;
+            int wcoeff = weights[posx];
+            int pcoeff = profits[posx];
+            wmin -= wcoeff * x->minimum();
+            pmin -= pcoeff * x->minimum();
+            long wmaxi = wmax - wcoeff * x->maximum();   // we remove the contribution of the variable we consider
+            long pmaxi = pmax - pcoeff * x->maximum();
+            dom.removeValues(LT, plimit - pmaxi, pcoeff);
+            if(weights[posx] == 0)
+                continue;
+            while(true) {
+                int v              = x->minimum();
+                int nPossibleMoves = (int)std::floor((pmaxi + pcoeff * v - plimit) / minProfit);   // 1 if 0 present ? why ?
+                if((wmaxi - nPossibleMoves * maxWeight) + wcoeff * v > wlimit) {
+                    if(solver->delVal(x, v) == false)
+                        return false;
+                } else
+                    break;
+            }
+            wmin += wcoeff * x->minimum();
+            pmin += pcoeff * x->minimum();
+            if(pmin >= plimit)
+                break;
+        }
+    }
+    return true;
+}
+
+
+bool KnapsackVARW::filter(Variable *dummy) {
+    recomputeBounds();
+    if(solver->delValuesLowerOrEqualThan(varWLimit, wmin - 1) == false)
+        return false;
+    wlimit = varWLimit->maximum();
+    return Knapsack::filter(dummy);
+}
+
+
+bool KnapsackVARP::filter(Variable *dummy) {
+    recomputeBounds();
+    if(solver->delValuesGreaterOrEqualThan(varPLimit, pmax + 1) == false)
+        return false;
+    plimit = varPLimit->minimum();
+    return Knapsack::filter(dummy);
+}
+
+bool KnapsackVARWP::filter(Variable *dummy) {
+    recomputeBounds();
+    if(solver->delValuesLowerOrEqualThan(varWLimit, wmin - 1) == false)
+        return false;
+    if(solver->delValuesGreaterOrEqualThan(varPLimit, pmax + 1) == false)
+        return false;
+    wlimit = varWLimit->maximum();
+    plimit = varPLimit->minimum();
+    return Knapsack::filter(dummy);
+}
+
+
+//----------------------------------------------
+// Construction and initialisation
+//----------------------------------------------
+
+Knapsack::Knapsack(Problem &p, std::string n, vec<Variable *> &v, vec<int> &ws, vec<int> &ps, int wl, int pl)
+    : GlobalConstraint(p, n, v, "Knapsack") {
+    v.copyTo(vars);
+    ws.copyTo(weights);
+    ps.copyTo(profits);
+    wlimit         = wl;
+    plimit         = pl;
+    basic          = true;
+    this.minWeight = weights.min();
+    this.maxWeight = weights.max();
+    this.minProfit = profits.min();
+    this.maxProfit = profits.max();
+}
+
+KnapsackVARW::KnapsackVARW(Problem &p, std::string n, vec<Variable *> &v, vec<int> &ws, vec<int> &ps, Variable *wl, int pl)
+    : Knapsack(p, n, v, ws, ps, 0, pl) {
+    addToScope(wl);
+    varWLimit = wl;
+    basic     = false;
+}
+
+KnapsackVARP::KnapsackVARP(Problem &p, std::string n, vec<Variable *> &v, vec<int> &ws, vec<int> &ps, int wl, Variable *pl)
+    : Knapsack(p, n, v, ws, ps, wl, 0) {
+    addToScope(pl);
+    varPLimit = pl;
+    basic     = false;
+}
+
+KnapsackVARWP::KnapsackVARWP(Problem &p, std::string n, vec<Variable *> &v, vec<int> &ws, vec<int> &ps, Variable *wl,
+                             Variable *pl)
+    : Knapsack(p, n, v, ws, ps, 0, 0) {
+    addToScope(wl);
+    addToScope(pl);
+    varPLimit = pl;
+    varWLimit = wl;
+    basic     = false;
+}
---
+++ constraints/globals/packing/Knapsack.h
@@ -0,0 +1,74 @@
+//
+// Created by audemard on 27/03/2026.
+//
+
+#ifndef COSOCO_KNAPSACK_H
+#define COSOCO_KNAPSACK_H
+#include "GlobalConstraint.h"
+
+namespace Cosoco {
+class Knapsack : public GlobalConstraint {
+   protected:
+    vec<Variable *> vars;
+    vec<int>        weights;
+    vec<int>        profits;
+
+    int  wlimit;
+    int  plimit;
+    long wmin, wmax;
+    long pmin, pmax;
+    int  minWeight, maxWeight;
+    int  minProfit, maxProfit;
+    bool basic;
+
+   public:
+    Knapsack(Problem &p, std::string n, vec<Variable *> &vars, vec<int> &w, vec<int> &p, int wl, int pl);
+    bool isCorrectlyDefined() override;
+
+
+    // Filtering method, return false if a conflict occurs
+    void recomputeBounds();
+    bool filter(Variable *x) override;
+    // Checking
+    bool isSatisfiedBy(vec<int> &tuple) override;
+};
+
+class KnapsackVARW : public Knapsack {
+   protected:
+    Variable *varWLimit;
+
+   public:
+    KnapsackVARW(Problem &p, std::string n, vec<Variable *> &vars, vec<int> &w, vec<int> &p, Variable *wl, int pl);
+    // Filtering method, return false if a conflict occurs
+    bool filter(Variable *x) override;
+    // Checking
+    bool isSatisfiedBy(vec<int> &tuple) override;
+};
+
+class KnapsackVARP : public Knapsack {
+   protected:
+    Variable *varPLimit;
+
+   public:
+    KnapsackVARP(Problem &p, std::string n, vec<Variable *> &vars, vec<int> &w, vec<int> &p, int wl, Variable *pl);
+    // Filtering method, return false if a conflict occurs
+    bool filter(Variable *x) override;
+    // Checking
+    bool isSatisfiedBy(vec<int> &tuple) override;
+};
+
+class KnapsackVARWP : public Knapsack {
+   protected:
+    Variable *varPLimit;
+    Variable *varWLimit;
+
+
+   public:
+    KnapsackVARWP(Problem &p, std::string n, vec<Variable *> &vars, vec<int> &w, vec<int> &p, Variable *wl, Variable *pl);
+    // Filtering method, return false if a conflict occurs
+    bool filter(Variable *x) override;
+    // Checking
+    bool isSatisfiedBy(vec<int> &tuple) override;
+};
+}   // namespace Cosoco
+#endif   // COSOCO_KNAPSACK_H
